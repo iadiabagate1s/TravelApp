e79485b37312c430eb12c56845c543e2e5864c24
Isaac Diabagate <iadiabagate1s@semo.edu>
2020-08-24 01:13:17 -0400

    man

--- blitz.config.js
+++ blitz.config.js
@@ -6,12 +6,18 @@ module.exports = {
       unstable_isAuthorized: unstable_simpleRolesIsAuthorized,
     }),
   ],
-  /* Uncomment this to customize the webpack config
+  
   webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
     // Note: we provide webpack above so you should not `require` it
     // Perform customizations to webpack config
     // Important: return the modified config
+    config.plugins.push(new webpack.IgnorePlugin(/\/__tests__\//))
     return config
+    
   },
-  */
+  webpackDevMiddleware: (config) => {
+    // Perform customizations to webpack dev middleware config
+    // Important: return the modified config
+    return config
+  
 }
